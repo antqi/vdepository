e114be3cd36b575fea8837827be43e60c63f655c
antqi <hi.antqi@gmail.com>
2018-07-06 17:07:25 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Demo from '@/pages/demo'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'demo',
+      component: Demo
+    }
+  ]
+})
